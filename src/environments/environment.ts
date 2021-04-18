9067a57b3f55036428c21d66e06f227c3da528a9
Nagendra-UI <67358077+Nagendra-UI@users.noreply.github.com>
2021-04-18 10:20:18 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAyqicUVJWfwX3mWDwZ-Nvr30D-2HkK5ww",
+    apiKey: "****",
     authDomain: "ng-fitness-tracker-c21cd.firebaseapp.com",
     projectId: "ng-fitness-tracker-c21cd",
-    storageBucket: "ng-fitness-tracker-c21cd.appspot.com",
-    messagingSenderId: "1000214369834",
-    appId: "1:1000214369834:web:7ca97be584f2831e971553"
+    storageBucket: "",
+    messagingSenderId: "*****",
+    appId: "*****"
   }
 };
 
